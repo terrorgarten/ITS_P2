0eb0800841de86fdc76b367b1f7895ebb8de7423
Matěj <xkonop03@vutbr.cz>
2023-04-20 01:51:53 +0200

    new utils.py procedures

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
+# import generalization for all features
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import random
+import string
+import time
 
 # generally useful buttons
 BASKET_BTN = '.btn-inverse'
 HOME_BTN = '#logo .img-fluid'
+
+ADMIN_USERNAME = "user"
+ADMIN_PASSWORD = "bitnami"
 
 # procedures for checking elements/links
 def check_exists_by_xpath(xpath: str, driver: webdriver) -> bool:
@@ -36,3 +43,22 @@ def check_link_exists_by_link_text(link_text: str, driver: webdriver) -> bool:
         print("utils: link by text NOT FOUND")
         return False
     return True
+
+def get_random_string(length: int) -> str:
+    """
+    Generates random string of <length> characters
+    :param length: String length
+    :return string:
+    """
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return str(result_str)
+
+def open_my_account_menu(driver: webdriver) -> None:
+    """
+    Custom function to use some html magic to hack the dropdown of My Account opening, as it can not be opened by selenium click()
+    :param driver: The webdriver. pass active context.driver object.
+    :return: None
+    """
+    toggler = driver.find_element(By.XPATH, '//*[@id="top"]/div/div[2]/ul/li[2]/div/ul')
+    driver.execute_script("arguments[0].setAttribute('class','dropdown-menu dropdown-menu-right show')", toggler)
